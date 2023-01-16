5387341b13d6f2441612455ea66916c53f6fe1ed
shahriar limon <shahriarwebdev@gmail.com>
2023-01-16 13:35:00 +0600

    implemented pagination in frontend

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,19 +1,23 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import "./Products.css";
 import Loader from '../../components/overlays/Loader/Loader';
 import { getProducts } from '../../redux/actions/productActions';
 import ProductCard from '../Home/ProductCart';
 import { useParams } from 'react-router-dom';
+import Pagination from "react-js-pagination";
 
 function Products() {
     const dispatch = useDispatch();
     const { keyword } = useParams()
-    console.log(keyword)
-    const { loading, error, products, productsCount } = useSelector((state) => state.products);
+    const { loading, error, products, productsCount, resultPerPage } = useSelector((state) => state.products);
+    const [currentPage, setCurrentPage] = useState(1)
+    const setCurrentPageNo = (e) => {
+        setCurrentPage(e)
+    }
     useEffect(() => {
-        dispatch(getProducts(keyword))
-    }, [keyword, dispatch])
+        dispatch(getProducts(keyword, currentPage))
+    }, [keyword, dispatch, currentPage])
     return (
         <Fragment>
 
@@ -25,6 +29,24 @@ function Products() {
                             products && products.map((product) => <ProductCard product={product} key={product?._id} />)
                         }
                     </div>
+                    {resultPerPage < productsCount && (
+                        <div className="paginationBox">
+                            <Pagination
+                                activePage={currentPage}
+                                itemsCountPerPage={resultPerPage}
+                                totalItemsCount={productsCount}
+                                onChange={setCurrentPageNo}
+                                nextPageText="Next"
+                                prevPageText="Prev"
+                                firstPageText="1st"
+                                lastPageText="Last"
+                                itemClass="page-item"
+                                linkClass="page-link"
+                                activeClass="pageItemActive"
+                                activeLinkClass="pageLinkActive"
+                            />
+                        </div>
+                    )}
 
 
                 </Fragment>)
--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,10 +1,10 @@
 import { ALL_PRODUCT_FAIL, ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, CLEAR_ERROR, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS } from "../actionTypes/productActionTypes";
 import axios from 'axios'
 
-export const getProducts = (keyword = "") => async (dispatch) => {
+export const getProducts = (keyword = "", currentPage = 1) => async (dispatch) => {
     try {
         dispatch({ type: ALL_PRODUCT_REQUEST });
-        let link = `http://localhost:5000/api/products?keyword=${keyword}`
+        let link = `http://localhost:5000/api/products?keyword=${keyword}&page=${currentPage}`
         const { data } = await axios.get(link, { withCredentials: true })
         dispatch({ type: ALL_PRODUCT_SUCCESS, payload: data })
     } catch (error) {
